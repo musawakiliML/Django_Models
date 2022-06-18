b2b295b1836caf59e961f31818c3c45001415c51
musawakiliML <musawakiliml@hotmail.com>
2022-06-19 00:09:23 +0100

    added post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+user = get_user_model()
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(user, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
